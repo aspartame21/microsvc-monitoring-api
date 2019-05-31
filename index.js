11f857523c16890fedf06cb73e83dc376ead7cef
Nijat <n.e.mahmudov@asan.gov.az>
2019-05-31 18:39:23 +0400

    Refactor

--- index.js
+++ index.js
@@ -2,50 +2,30 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
-// app.use(bodyParser);
+const LABEL_SELECTOR = "com.docker.compose.service";
 
-http.request({ socketPath: '/var/run/docker.sock', path: '/v1.24/containers/json' }, res => {
-  console.log(`STATUS: ${res.statusCode}`);
-  res.setEncoding('utf-8');
-  res.on('data', data => console.log(JSON.parse(data)[0]));
-  res.on('err', err => console.log(err));
-}).end();
-
-// const ls = spawn('ls', ['-lh', '/usr']);
-
-// ls.stdout.on('data', (data) => {
-//   console.log(`stdout: ${data}`);
-// });
-
-// ls.stderr.on('data', (data) => {
-//   console.log(`stderr: ${data}`);
-// });
-
-// ls.on('close', (code) => {
-//   console.log(`child process exited with code ${code}`);
-// });
-
-app.get('/', (req, res) => {
-  res.send('OK');
-});
 app.get('/api/v1/health/:service', async (req, res) => {
   const service = req.params.service;
-  const status = await isAlive(service);
-  res.send(status);
+  getService(service, container => { 
+    container = JSON.parse(container);
+    const state = container.length !== 0 ? container[0].State : "N/A";
+    state === "running" ? res.redirect(badgify(service, state, 'brightgreen')) : res.redirect(badgify(service, state, 'red'));
+  });
 });
-
-app.get('/api/v1/availability/:service', (req, res) => {
-  res.send("error")
-});
-
-
-
-isAlive = async service => {
-  return execSync(`docker ps | grep ${service}`, { stdio: "ignore" }).toString("utf-8")// === "" ? false : true;
-}
-
-imagify = status => {
-  throw new Error('Method is not implemented');
-}
 
 app.listen(process.env.NODE_PORT || 6767, () => console.log('Listening on 6767'));
+
+function badgify(label, message, color) {
+  return `https://img.shields.io/badge/${label.replace('-','--')}-${message.replace('-','--')}-${color}.svg`;
+}
+
+function getService(service, cb) {
+  http.request({
+    socketPath: '/var/run/docker.sock',
+    path: `/v1.24/containers/json?filters={"label":{"${LABEL_SELECTOR}=${service}":true}}`
+  }, res => {
+    res.setEncoding('utf-8');
+    res.on('data', data => cb(data));
+    res.on('err', err => cb(err));
+  }).end();
+}
